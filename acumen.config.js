37e9ae7a31bb621110ec89ad54f6127eabf8f2e6
Josh Miller <wickedhillsmedia@gmail.com>
2019-12-31 17:05:27 -0500

    added configuration files

---
+++ acumen.config.js
@@ -0,0 +1,26 @@
+const acumen = require("@ao-framework/acumen")
+
+module.exports = {
+    concepts: {
+        main(environment) {
+            acumen.conceptualize(environment)
+                .terminalEnvironment(terminal => {
+                    terminal.snapshot();
+                    terminal.repo.entry("dist/test/main.js")
+                })
+        },
+        dev(environment) {
+            acumen.conceptualize(environment)
+                .serverEnvironment(server => {
+                    server.repo.entry("dist/test/main.js")
+                    server.repo.watch("dist/test")
+                    server.repo.watch("dist/src")
+                    server.repo.coverageOptions({
+                        includeDirectories: ["dist/src"],
+                        excludeDirectories: ["dist/src/contracts"]
+                    })
+                    server.port(4000);
+                })
+        }
+    }
+}
