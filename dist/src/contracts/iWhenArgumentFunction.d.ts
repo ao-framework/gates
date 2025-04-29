572e136a5bdf6ec7da7e18fb8d4f5b78b48cb922
Josh Miller <josh.contractor@meowwolf.com>
2025-04-28 23:05:44 -0400

    Removing old tests and dist folder

--- dist/src/contracts/iWhenArgumentFunction.d.ts
+++
@@ -1,1 +0,0 @@
-export declare type iWhenArgumentFunction<Type> = (arg: Type) => any;
--- dist/test/suite.ensureFunctions.d.ts
+++
@@ -1,7 +0,0 @@
-import { Apparatus } from "@ao-framework/acumen";
-export declare class SuiteEnsureFunctions {
-    controller({ testAll }: Apparatus): Promise<any[]>;
-    ensureObject({ expect, spotlight }: Apparatus): void;
-    ensureArray({ expect }: Apparatus): void;
-    ensureFunction({ expect, tagger }: Apparatus): void;
-}
